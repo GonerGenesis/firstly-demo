68e40a1f463f0a1036d5fa299405b30c4131bb07
GonerGenesis <gunnar.kistner@gmail.com>
2024-07-09 10:55:33 +0200

    moep

---
+++ test.mjs
@@ -0,0 +1,5 @@
+import {print} from './node_modules/.pnpm/code-red@1.0.4/node_modules/code-red/src/print/index.js';
+console.log(print);
+
+// import { encode } from './node_modules/.pnpm/@jridgewell+sourcemap-codec@1.4.15/node_modules/@jridgewell/sourcemap-codec/dist/sourcemap-codec.umd.js';
+// console.log(encode);
